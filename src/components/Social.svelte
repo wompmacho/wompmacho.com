c697e97eefe3c02c4a15696439055692f6862b3c
wompmacho <wompmacho+github@gmail.com>
2020-11-06 06:30:42 -0500

    adding video cdn - git LFS

--- src/components/Social.svelte
+++ src/components/Social.svelte
@@ -1,12 +1,12 @@
 <social-links>
     <div class="social d-flex justify-content-center align-middle">
-        <a class="mx-2" href="#!"><i class="fab fa-discord fa-2x"></i></a>
-        <a class="mx-2" href="#!"><i class="fab fa-youtube fa-2x"></i></a>
-        <a class="mx-2" href="#!"><i class="fab fa-github fa-2x"></i></a>
-        <a class="mx-2" href="#!"><i class="fab fa-twitter fa-2x"></i></a>
-        <a class="mx-2" href="#!"><i class="fab fa-instagram fa-2x"></i></a>
-        <a class="mx-2" href="#!"><i class="fab fa-linkedin fa-2x"></i></a>
-        <a class="mx-2" href="#!"><i class="fab fa-facebook fa-2x"></i></a>
+        <a class="mx-2" href="https://discord.gg/pVNnTDA"><i class="fab fa-discord fa-2x"></i></a>
+        <a class="mx-2" href="https://www.youtube.com/wompmacho"><i class="fab fa-youtube fa-2x"></i></a>
+        <a class="mx-2" href="https://github.com/wompmacho"><i class="fab fa-github fa-2x"></i></a>
+        <a class="mx-2" href="https://twitter.com/wompmacho"><i class="fab fa-twitter fa-2x"></i></a>
+        <a class="mx-2" href="https://www.instagram.com/wompmacho/"><i class="fab fa-instagram fa-2x"></i></a>
+        <a class="mx-2" href="https://www.linkedin.com/in/wompmacho/"><i class="fab fa-linkedin fa-2x"></i></a>
+        <a class="mx-2" href="https://www.facebook.com/wompmacho"><i class="fab fa-facebook fa-2x"></i></a>
     </div>
 </social-links>
 
